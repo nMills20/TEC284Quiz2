214e7101a5961fa66f34925f5d3d0fb5b4c0457e
nMills20 <ngmills@ilstu.edu>
2025-04-10 14:32:17 -0500

    Add files via upload
    Created function for LED light button press

--- Quiz-2.py
+++ Quiz-2.py
@@ -9,3 +9,10 @@ led = RGBLED (red = 5, green = 6, blue = 26)
 red_button = Button(17)
 green_button = Button(27)
 blue_button = Button(22)
+
+#function for LED color
+def update_led():
+    r = 1 if red_button.is_pressed else 0
+    g = 1 if green_button.is_pressed else 0
+    b = 1 if blue_button.is_pressed else 0
+    led.color = (r,g,b)
